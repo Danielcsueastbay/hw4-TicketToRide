c1d8061e5ce6e32119efb0adde13b1587b45fceb
Daniel <Daniel@DESKTOP-S015Q0T>
2018-10-20 20:31:21 -0700

    skelly code of junittest added

---
+++ test/test/JUnitTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class JUnitTest {
+    //skelly code
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+
+    @Test
+    public void add() {
+        //TicketCardDeck.java 
+        //test if amount of ticket cards returned to bottom of ticket deck is 0-2
+         
+    }
+    @Test
+    public void add1() {
+        //DiscardPile.java
+        //test if discarded traincard array is empty
+        
+    }
+    @Test
+    public void add2() {
+        //DiscardPile.java
+        //test if train cards deck recieved the discarded train cards from discard pile
+
+        
+    }
+    @Test
+    public void remove1() {
+        //DiscardPile.java
+        //test if used train cards in hand are removed
+        
+    }
+    @Test
+    public void remove2() {
+        //DiscardPile.java 
+        //test if removed train card is in discard pile 
+        
+    }
+
+    
+    
+    
+            
+            
+}
