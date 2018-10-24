62ef71abb11345390c4ef117a581a59252b44a9d
Daniel <Daniel@DESKTOP-S015Q0T>
2018-10-23 22:21:51 -0700

    some of the code updated

--- src/Model/TicketCard.java
+++ src/Model/TicketCard.java
@@ -14,11 +14,20 @@ public class TicketCard {
     //skelly code of TicketCard.java
     public int value;    //stores card’s value
     public int loc1, loc2;    //stores int location 
-    boolean completed;
-
-    public TicketCard(int card){
+    //public static boolean completed;
+    public TicketCard(){
+        loc1 = 1;
+        loc2 = 2;
+        value = 1;
+       // completed = true;
+    }
+    public TicketCard(int location1, int location2, int values){
         // set ticket card from the list of created Ticketcards from TicketCardDeck.java
-        //int card is card location from created list of cards from TicketCardDeck.java
+        loc1 = location1;
+        loc2 = location2;
+        value = values;
+        //completed = done;
+        
     }
     public int getValue() {
         
@@ -32,7 +41,19 @@ public class TicketCard {
         
         return loc2;
     }    
-    
+ 
+    @Override
+    public String toString(){
+        return "City " + loc1 + " to City " + loc2 + " is: " + value;
+    }
+    /*
+    //test print for TicketCard
+    public static void main(String[] args)  {
+        TicketCard tc = new TicketCard();
+        System.out.print(tc.toString() + "\n");
+        //run: City 1 to City 2 is: 1
+   }
+    */
 }
 
 
